9aa12efe3e79668cdc23ac6315dd486788f6f66c
DennerOl <dener155@hotmail.com>
2023-05-23 22:49:13 -0300

    Criado o projeto e classes Order, User

---
+++ src/main/java/com/devsuperior/dscomercio/DscomercioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscomercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscomercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscomercioApplication.class, args);
+	}
+
+}
