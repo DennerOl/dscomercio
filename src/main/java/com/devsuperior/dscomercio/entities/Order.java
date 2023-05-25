75de58955f907e5c7c52840e9cf9f18a055b4a07
DennerOl <dener155@hotmail.com>
2023-05-24 21:41:32 -0300

    Payment

--- src/main/java/com/devsuperior/dscomercio/entities/Order.java
+++ src/main/java/com/devsuperior/dscomercio/entities/Order.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscomercio.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,16 +20,76 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")	
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private OrderStatus status;
 
-/* um pedido tem que ter um usuario	
- * ou seja o usuario pode ter varios pedidos
- * (lado do muitos)
- */
+	/*
+	 * um pedido tem que ter um usuario ou seja o usuario pode ter varios pedidos
+	 * (lado do muitos)
+	 */
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	/*
+	 * um pedido pode ter um payment ou não ou seja se eu excluir um pedido não faz
+	 * sentido ter um payment
+	 */
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
+	public Order() {
+
+	}
+
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 }
