954f6866002e4b42f1f4df5fee7059cf171014dc
DennerOl <dener155@hotmail.com>
2023-06-05 21:50:55 -0300

    Tratando exceção com resposta customizada

---
+++ src/main/java/com/devsuperior/dscomercio/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscomercio.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dscomercio.dto.CustomError;
+import com.devsuperior.dscomercio.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+	public class ControllerExceptionHandler {
+	
+/* metodo que vai tratar a exceção ResourceNotFoundException
+ * httpServlet- pega a url que deu a exceção para tratar
+ * status foi convertido 
+ */
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+	HttpStatus status = HttpStatus.NOT_FOUND;
+	// chamo CustomErro passando os atributos obtidos na URL
+	CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	
+	}
+}
+
