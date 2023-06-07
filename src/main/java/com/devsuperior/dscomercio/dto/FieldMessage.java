5aa0b0e872b599402b545b947540cea9565b0f78
DennerOl <dener155@hotmail.com>
2023-06-06 21:49:08 -0300

    Bean Validation

---
+++ src/main/java/com/devsuperior/dscomercio/dto/FieldMessage.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dscomercio.dto;
+
+public class FieldMessage {
+/*Classe responsável por customizar as mensagens 
+ * de exceção das anotações do productDTO
+ */
+	
+	private String fieldName;
+	private String message;
+	
+	
+	public FieldMessage(String fieldName, String message) {
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+	
+}
