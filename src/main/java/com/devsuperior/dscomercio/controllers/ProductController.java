b2329f96af3bb84ed880b0d6a434ffb2525da956
DennerOl <dener155@hotmail.com>
2023-07-01 19:58:16 -0300

    Consulta customizada por nome JPQL

--- src/main/java/com/devsuperior/dscomercio/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscomercio/controllers/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -43,10 +44,14 @@ public class ProductController {
 /*
  * metodo para retornar a lista de produtos paginada através do service
  * (caso de uso -> consultar catalago)
+ * url para consulta customizada JPQL
+ * http://localhost:8080/products?size=12&page=0&sort=name,desc&name=mac
  */
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-		Page<ProductDTO> dto = service.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findAll(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			Pageable pageable) {
+		Page<ProductDTO> dto = service.findAll(name, pageable);
 		return ResponseEntity.ok(dto);
 	}
 
